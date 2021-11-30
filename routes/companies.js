5de67f905aee5c8f7ab869df40fcac55ab92e324
lorenshadi98 <lorans.hadi@gmail.com>
2021-11-30 14:40:53 -0800

    Completed biztime exercise

--- routes/companies.js
+++ routes/companies.js
@@ -3,32 +3,40 @@ const router = new express.Router();
 const db = require('../db');
 const ExpressError = require('../expressError');
 
+// returns list of all companies
 router.get('/', async (req, res, next) => {
   try {
     const results = await db.query(`SELECT * FROM companies`);
-    return res.json({ companies: [results.rows] });
+    return res.json({ companies: results.rows });
   } catch (err) {
     return next(err);
   }
 });
 
+// returns info on specified company based on company code
 router.get('/:code', async (req, res, next) => {
   try {
     const company_code = req.params.code;
     const result = await db.query(`SELECT * FROM companies WHERE code =$1`, [
       company_code
     ]);
+
     if (result.rows.length === 0) {
       return res
         .status(404)
         .send(new ExpressError('Cannot find specified company', 404));
     }
-    return res.json({ company: [result.rows] });
+    const invoices = await db.query(
+      `SELECT * FROM invoices WHERE comp_code=$1`,
+      [company_code]
+    );
+    return res.json({ company: result.rows, invoices: invoices.rows });
   } catch (err) {
     return next(err);
   }
 });
 
+// Creates a new company
 router.post('/', async (req, res, next) => {
   try {
     const { code, name, description } = req.body;
@@ -42,6 +50,7 @@ router.post('/', async (req, res, next) => {
   }
 });
 
+// If company exists based on give company code, then update company with new json info
 router.put('/:code', async (req, res, next) => {
   try {
     const company_code = req.params.code;
@@ -61,6 +70,7 @@ router.put('/:code', async (req, res, next) => {
   }
 });
 
+// If comaony exists, delete company
 router.delete('/:code', async (req, res, next) => {
   try {
     const company_code = req.params.code;
--- routes/invoices.js
+++ routes/invoices.js
@@ -1,4 +1,92 @@
 const express = require('express');
 const router = new express.Router();
+const db = require('../db');
+const ExpressError = require('../expressError');
+
+// Returns all invoices
+router.get('/', async (req, res, next) => {
+  try {
+    const results = await db.query(`SELECT * FROM invoices`);
+    return res.json({ invoices: [results.rows] });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+// Returns obj on given invoice.
+router.get('/:id', async (req, res, next) => {
+  try {
+    const invoice_id = req.params.id;
+    const invoices = await db.query(`SELECT * FROM invoices WHERE id=$1 `, [
+      invoice_id
+    ]);
+    if (invoices.rows.length === 0) {
+      return res
+        .status(404)
+        .send(new ExpressError('Cannot find specified invoice', 404));
+    }
+    found_comp = invoices.rows[0].comp_code;
+    const company = await db.query(`SELECT * FROM companies WHERE code=$1`, [
+      found_comp
+    ]);
+
+    return res.json({ invoice: invoices.rows, company: company.rows });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+//Adds an invoice.
+router.post('/', async (req, res, next) => {
+  try {
+    const { comp_code, amt } = req.body;
+    const result = await db.query(
+      `INSERT INTO invoices(comp_code, amt) VALUES($1,$2) RETURNING id, comp_code, amt, paid, add_date, paid_date`,
+      [comp_code, amt]
+    );
+    return res.json({ invoice: result.rows });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+// modifies an invoice
+router.put('/:id', async (req, res, next) => {
+  try {
+    const invoice_id = req.params.id;
+    const { amt } = req.body;
+    const result = await db.query(
+      `UPDATE invoices SET amt=$1 WHERE id =$2 RETURNING id, comp_code, amt, paid, add_date, paid_date`,
+      [amt, invoice_id]
+    );
+    if (result.rows.length === 0) {
+      return res
+        .status(404)
+        .send(new ExpressError('Cannot find specified invoice', 404));
+    }
+    return res.json({ invoice: result.rows });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+//deletes an invoice
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const invoice_id = req.params.id;
+    const result = await db.query(
+      `DELETE FROM invoices WHERE id = $1 RETURNING id, comp_code, amt`,
+      [invoice_id]
+    );
+    if (result.rows.length === 0) {
+      return res
+        .status(404)
+        .send(new ExpressError('Cannot find specified invoice', 404));
+    }
+    return res.json({ status: 'deleted' });
+  } catch (err) {
+    return next(err);
+  }
+});
 
 module.exports = router;
