06df54b6049f9b21cae763d45b9062c5fb0255e0
lorenshadi98 <lorans.hadi@gmail.com>
2021-11-30 13:49:19 -0800

    completed company routes

--- routes/companies.js
+++ routes/companies.js
@@ -42,4 +42,41 @@ router.post('/', async (req, res, next) => {
   }
 });
 
+router.put('/:code', async (req, res, next) => {
+  try {
+    const company_code = req.params.code;
+    const { name, description } = req.body;
+    const result = await db.query(
+      `UPDATE companies SET name=$1, description=$2 WHERE code =$3 RETURNING code, name, description`,
+      [name, description, company_code]
+    );
+    if (result.rows.length === 0) {
+      return res
+        .status(404)
+        .send(new ExpressError('Cannot find specified company', 404));
+    }
+    return res.json({ company: result.rows });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+router.delete('/:code', async (req, res, next) => {
+  try {
+    const company_code = req.params.code;
+    const result = await db.query(
+      `DELETE FROM companies WHERE code = $1 RETURNING code, name, description`,
+      [company_code]
+    );
+    if (result.rows.length === 0) {
+      return res
+        .status(404)
+        .send(new ExpressError('Cannot find specified company', 404));
+    }
+    return res.json({ status: 'deleted' });
+  } catch (err) {
+    return next(err);
+  }
+});
+
 module.exports = router;
